d864cb08e59ac7cec30a8e71727aa3657ee0d86c
Tu <tu.39094@gmail.com>
2025-07-23 19:59:06 -0400

    Layoffs_DataCleaningProject.sql

--- Layoffs/Layoffs_DataCleaningProject.sql
+++ Layoffs/Layoffs_DataCleaningProject.sql
@@ -1,10 +1,16 @@
 -- Tech Layoffs - Data Cleaning Project
 -- https://www.kaggle.com/datasets/swaptr/layoffs-2022
 
+-- Data Cleaning Guideline:
+-- 1. Remover dulicates (if any)
+-- 2. Standardize data and fix errors
+-- 3. Handle NULL or blank values
+-- 4. Remove any rows or columns that aren't necessary
+
+
 SELECT * 
 FROM world_layoffs.layoffs
 ;
-
 
 -- Create a staging table
 
@@ -17,14 +23,9 @@ SELECT * FROM world_layoffs.layoffs
 ;
 
 
--- now when we are data cleaning we usually follow a few steps
--- 1. check for duplicates and remove any
--- 2. standardize data and fix errors
--- 3. Look at null values and see what 
--- 4. remove any columns and rows that are not necessary - few ways
-
 
 -- 1. Remove Duplicates
+
 
 SELECT *
 FROM world_layoffs.layoffs_staging
@@ -140,7 +141,6 @@ FROM world_layoffs.layoffs_staging2
 ;
 
 -- Inserting new relevant column (row_num) for duplicates.
-
 INSERT INTO layoffs_staging2
 SELECT *,
 ROW_NUMBER() OVER(PARTITION BY
@@ -159,14 +159,13 @@ FROM world_layoffs.layoffs_staging
 ;
 
 -- Reconfirming duplicates in the new table.
-
+-- Again, if row_num is > 1, that indicates there are duplicates.
 SELECT *
 FROM world_layoffs.layoffs_staging2
 WHERE row_num > 1
 ;
 
 -- Duplicates confirmed. Now deleting duplicates.
-
 DELETE
 FROM world_layoffs.layoffs_staging2
 WHERE row_num > 1
@@ -178,7 +177,6 @@ WHERE row_num > 1
 
 
 -- After investigating data, blank whitespaces are discovered in company names.
-
 SELECT * 
 FROM world_layoffs.layoffs_staging2
 ;
@@ -188,7 +186,6 @@ FROM world_layoffs.layoffs_staging2
 ;
 
 -- Trimming evident whitespaces in company names and updating the table.
-
 SELECT
 	company,
 	TRIM(company)
@@ -199,8 +196,8 @@ UPDATE world_layoffs.layoffs_staging2
 SET company = TRIM(company)
 ;
 
--- Multiple names for Crpto/Cryptocurrency discovered in 'industry'.
 
+-- Multiple names for Crpto/Cryptocurrency discovered in 'industry'.
 SELECT DISTINCT industry
 FROM world_layoffs.layoffs_staging2
 ORDER BY industry
@@ -211,100 +208,197 @@ FROM world_layoffs.layoffs_staging2
 WHERE industry LIKE 'Crypto%'
 ;
 
--- Updating multiple Crypto/cryptocurrency names to just 'Crypto'.
-
+-- Updating multiple Crypto/cryptocurrency names to just "Crypto".
 UPDATE world_layoffs.layoffs_staging2
 SET industry = 'Crypto'
 WHERE industry LIKE 'Crypto%'
 ;
 
--- Confirming names are updated to labeled as just "Crypto".
-
+-- Confirming names are updated to just "Crypto".
 SELECT DISTINCT industry
 FROM world_layoffs.layoffs_staging2
 ORDER BY 1
 ;
 
 
+-- Duplicate/misspelling found in country column for "United States". 
+-- Some "United States" and some "United States." with a period at the end.
+SELECT DISTINCT country
+FROM world_layoffs.layoffs_staging2
+ORDER BY 1
+
+SELECT *
+FROM layoffs_staging2
+WHERE country LIKE 'United States%'
+;
+	
+SELECT DISTINCT
+	country,
+	TRIM(TRAILING '.' FROM country)
+FROM world_layoffs.layoffs_staging2
+ORDER BY 1
+;
+
+-- Updating duplicate/misspellings to just "United States"
+UPDATE world_layoffs.layoffs_staging2
+SET country = TRIM(TRAILING '.' FROM country)
+WHERE country LIKE 'United States%'
+;
+
+-- Change confirmed.
+SELECT DISTINCT location
+FROM world_layoffs.layoffs_staging2
+ORDER BY 1
+;
 
 
--- --------------------------------------------------- completion line
-
-
-
-
--- we also need to look at 
-
+-- Investigating further...
 SELECT *
 FROM world_layoffs.layoffs_staging2;
 
--- everything looks good except apparently we have some "United States" and some "United States." with a period at the end. Let's standardize this.
-SELECT DISTINCT country
+
+-- Problem in date column found.
+-- Date is a text data type. Needs to be converted to the proper format and data type.
+SELECT 
+	`date`,
+	str_to_date(`date`, '%m/%d/%Y')
 FROM world_layoffs.layoffs_staging2
-ORDER BY country;
+;
+
+-- Updating table with the proper date format...
+UPDATE world_layoffs.layoffs_staging2
+SET `date` = str_to_date(`date`, '%m/%d/%Y')
+;
+
+-- Confirming update.
+SELECT `date`
+FROM world_layoffs.layoffs_staging2
+;
+
+-- Now updating data type from text to date.
+ALTER TABLE world_layoffs.layoffs_staging2
+MODIFY COLUMN `date` DATE
+;
+
+
+-- Discovered 'funds_raised millions" is a also a text data type.
+-- funds_raised_millions column needs to converted into an integer data type.
+SELECT funds_raised_millions
+FROM layoffs_staging2
+ORDER BY 1 DESC;
 
 UPDATE layoffs_staging2
-SET country = TRIM(TRAILING '.' FROM country);
+SET funds_raised_millions = NULL
+WHERE funds_raised_millions = 'NULL';
 
--- now if we run this again it is fixed
-SELECT DISTINCT country
-FROM world_layoffs.layoffs_staging2
-ORDER BY country;
-
-
--- Let's also fix the date columns:
-SELECT *
-FROM world_layoffs.layoffs_staging2;
-
--- we can use str to date to update this field
-UPDATE layoffs_staging2
-SET `date` = STR_TO_DATE(`date`, '%m/%d/%Y');
-
--- now we can convert the data type properly
+-- Now updating data type from text to an integer.
 ALTER TABLE layoffs_staging2
-MODIFY COLUMN `date` DATE;
+MODIFY COLUMN funds_raised_millions INT;
 
-
-SELECT *
-FROM world_layoffs.layoffs_staging2;
-
-
-
-
-
--- 3. Look at Null Values
-
--- the null values in total_laid_off, percentage_laid_off, and funds_raised_millions all look normal. I don't think I want to change that
--- I like having them null because it makes it easier for calculations during the EDA phase
-
--- so there isn't anything I want to change with the null values
-
-
-
-
--- 4. remove any columns and rows we need to
-
-SELECT *
+-- Confirming update.
+SELECT funds_raised_millions
 FROM world_layoffs.layoffs_staging2
-WHERE total_laid_off IS NULL;
+;
 
 
+
+-- 3. NULL/Blank Values
+
+
+-- Inspecting nulls in total_laid_off and percentage_laid_off column.
 SELECT *
 FROM world_layoffs.layoffs_staging2
 WHERE total_laid_off IS NULL
 AND percentage_laid_off IS NULL;
 
--- Delete Useless data we can't really use
-DELETE FROM world_layoffs.layoffs_staging2
+-- Inspecting nulls and blanks in industry column.
+SELECT *
+FROM world_layoffs.layoffs_staging2
+WHERE industry IS NULL
+OR industry = ''
+;
+
+-- Discovered row where 'industry' cell is blank for Airbnb. Looking for other Airbnb rows where 'industry' cell is filled in for reference.
+SELECT *
+FROM world_layoffs.layoffs_staging2
+WHERE company = 'Airbnb'
+;
+
+-- Discovered other Airbnb rows where 'industry' cell is filled in.
+-- Attempting to populate NULL/Blank Values for Airbnb and other companies with known data.
+SELECT *
+FROM world_layoffs.layoffs_staging2 AS t1
+JOIN world_layoffs.layoffs_staging2 AS t2
+	ON t1.company = t2.company
+    AND t1.location = t2.location
+WHERE (t1.industry IS NULL OR t1.industry = '')
+AND t2.industry IS NOT NULL
+;
+
+UPDATE world_layoffs.layoffs_staging2 AS t1
+JOIN world_layoffs.layoffs_staging2 AS t2
+	ON t1.company = t2.company
+SET t1.industry = t2.industry
+WHERE (t1.industry IS NULL OR t1.industry = '')
+AND t2.industry IS NOT NULL
+;
+
+-- Update had not effect.
+-- Concluded that blanks to must be turned into NULL values, due to issues when updating with a WHERE clause that has an OR.
+UPDATE world_layoffs.layoffs_staging2
+SET industry = NULL
+WHERE industry = ''
+;
+
+-- Attempting update again.
+UPDATE world_layoffs.layoffs_staging2 AS t1
+JOIN world_layoffs.layoffs_staging2 AS t2
+	ON t1.company = t2.company
+SET t1.industry = t2.industry
+WHERE t1.industry IS NULL
+AND t2.industry IS NOT NULL
+;
+
+-- Update confirmed.
+SELECT *
+FROM world_layoffs.layoffs_staging2
+WHERE company = 'Airbnb'
+;
+
+
+
+-- 4. Removing rows and columns that aren't necessary.
+
+
+-- Investigating if rows should be deleted or not because both total_laid_off and percentage_laid_off columns are NULL.
+SELECT *
+FROM world_layoffs.layoffs_staging2
 WHERE total_laid_off IS NULL
-AND percentage_laid_off IS NULL;
+AND percentage_laid_off IS NULL
+;
 
-SELECT * 
-FROM world_layoffs.layoffs_staging2;
+-- Decision to delete rows where total_laid_off and percentage_laid_off columns are NULL.
+-- Rows have no value to us because relevant values are NULL.
+DELETE
+FROM world_layoffs.layoffs_staging2
+WHERE total_laid_off IS NULL
+AND percentage_laid_off IS NULL
+;
 
-ALTER TABLE layoffs_staging2
-DROP COLUMN row_num;
+-- Row deletion confirmed!
+SELECT *
+FROM world_layoffs.layoffs_staging2
+WHERE total_laid_off IS NULL
+AND percentage_laid_off IS NULL
+;
 
 
-SELECT * 
-FROM world_layoffs.layoffs_staging2;
+-- row_num column created earlier is no longer needed and should be deleted.
+ALTER TABLE world_layoffs.layoffs_staging2
+DROP COLUMN row_num
+;
+
+-- row_numn column deletion confirmed!
+SELECT *
+FROM world_layoffs.layoffs_staging2
+;
